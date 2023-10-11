aa8d031a90ac27520247b329441598822941747f
GuiSteinn <guilhermezuninosgrott@gmail.com>
2023-10-11 19:02:09 -0300

    ok

--- IntroJS/tabuada.js
+++
@@ -1,7 +0,0 @@
-//Comandos Javascript que serão executados na aplicação
-document.write("<strong>Linha escrita pelo JS externo</strong>");
-console.log("Arquivo JS externo executado");
-let nome = window.prompt("Qual seu nome?");
-window.alert("Olá "+nome);
-
-document.write("oiiii")
